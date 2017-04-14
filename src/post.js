448af4bf5fe8015c911063ba16bf0d973c4e13ff
frol <frol.mr@ya.ru>
2017-04-14 23:13:58 +0300

    news array have been passed to post component

--- src/post.js
+++ src/post.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Post extends React.Component {
   render() {
-    return <h3>Супер новость</h3>;
+    return (
+       <li>
+         <h4>{this.props.name}</h4>
+         <p>{this.props.description}</p>
+       </li>
+      );
   }
 }
+
+Post.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
