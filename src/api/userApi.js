10ab0f5bfadbd66a5714bd2a20867980887c93d2
Anton Frolov <frol.mr@ya.ru>
2017-11-18 19:13:44 +0300

    added endpoints to user api

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -13,4 +13,27 @@ export default class UserApi {
       return error
     });
   }
+
+  static getUser(key) {
+    return axios.get(process.env.REMOTE_HOST + '/api/user',
+      { headers: { Authorization: "Bearer " + key }})
+      .then(response => {
+        return response.data;
+      }).catch(error => {
+        return error;
+      });
+  }
+
+  static updateUser(key, user) {
+    return axios.post(process.env.REMOTE_HOST + '/api/update_user', {
+      "user": {
+        "locale": user.locale
+      }},
+      { headers: { Authorization: "Bearer " + key }})
+      .then(response => {
+        return response.data;
+      }).catch(error => {
+        return error;
+      });
+  }
 }
