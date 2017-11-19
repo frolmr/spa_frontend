6823b5c2763bb2099fbd21c1942e0695f18c8790
Anton Frolov <frol.mr@ya.ru>
2017-11-19 11:55:48 +0300

    user actions added

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -10,6 +10,34 @@ export const loginUser = (user) => {
   }
 }
 
+export const getUser = (key) => {
+  return function(dispatch) {
+    return userApi.getUser(key).then(user => {
+      dispatch(getUserSuccess(user));
+    }).catch(error => {
+      throw(error);
+    });
+  }
+}
+
+export const updateUser = (key, user) => {
+  return function(dispatch) {
+    return userApi.updateUser(key, user).then(user => {
+      dispatch(updateUserSuccess(user));
+    }).catch(error => {
+      throw(error);
+    });
+  }
+}
+
 export const loginUserSuccess = (user) => {
   return {type: 'LOGIN_USER_SUCCESS', user}
+}
+
+export const getUserSuccess = (user) => {
+  return {type: 'GET_USER_SUCCESS', user}
+}
+
+export const updateUserSuccess = (user) => {
+  return {type: 'UPDATE_USER_SUCCESS', user}
 }
